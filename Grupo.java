faa7e3efe8c5a95d1580b7950cd6bc8a284df542
omar navarro <omm27@hotmail.com>
2017-08-29 10:56:07 -0500

    se hizo con metodos pequeños para resolver un metodo mas grande como fue buscar alumno buscar posicion disponible y con eso ayudar a el metodo de inscribir a hacerlo mas efeciente.

--- Grupo.java
+++ Grupo.java
@@ -9,17 +9,46 @@ public class Grupo
         this.nombreMateria=nomMateria;
     }
     
-    public void inscribir(Estudiante unestudiante)
+    /**
+     * busca un espacio disponible en el arreglo.
+     * regresa @return una posicion nula en el arreglo.
+     * 
+     */
+    private int buscaEspacioDip()
     {
-        for (int i=0;i<estudiantes.length;i++)
+        for(int i=0;i<estudiantes.length;i++)
         {
             if (estudiantes[i]==null)
             {
-                estudiantes[i]=unestudiante;
-                break;
+                return i;
             }
         }
+        return -1;
     }
+    
+    /**
+     * inscribe un estudiante nuevo en el grupo.
+     * @param un estudiantees el objeto estudiante a inscribir a el grupo.
+     * @return regresa verdadero su el estudiante fue inscrito o falso en caso
+     * de que no se puede inscribir.
+     */
+    public boolean inscribir(Estudiante unestudiante)
+    {
+        int existe=this.buscarEs(unestudiante.Darclave());
+        
+        if (existe==-1)
+        {
+            return false;//el alumno ya esta inscrito.
+        }
+        int posDip= this.buscaEspacioDip();
+        if(posDip!=-1)
+        {
+            return false; //el arreglo esta lleno
+        }
+        estudiantes[posDip]=unestudiante;//inscribe un alumno
+        return true; 
+    }            
+          
     public void darBaja(int claveAlum)
     {
         for (int i=0;i<estudiantes.length;i++)
@@ -31,9 +60,22 @@ public class Grupo
             }
         }
     }
+    
+    /**
+       *busca un estudiante mediante el parametro @param claveAlum 
+       *y regresa el posicion en donde se encuentra ese alumno
+       *o -1 si no existe.
+    */
+   private int buscarEs(int claveAlum)
+    
+    {
+        for(int i=0;i<estudiantes.length;i++)
+        {
+            if(estudiantes[i].Darclave()==claveAlum)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
-   //dar de baja a un estudiante 
-   /*public void dabaja(int claveESTU)
-   {
-       
-   }*/
